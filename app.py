fbbf97b1f1e09a3d3b73e0f2e440be8ac5de633f
Jack-Liu0227 <2802644093@qq.com>
2024-10-29 17:10:06 +0800

    更新requirements

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 import os
 import streamlit as st
 from multipage import MultiPage
-from Pages import KappaP, home, AI4Kappa,CGCNN
+from Pages import KappaP, home, AI4Kappa
 
 st.set_page_config(page_title="Lattice Thermal Conductivity APP", page_icon=":evergreen_tree:", layout="wide")
 st.title('Lattice Thermal Conductivity APP')
@@ -12,10 +12,9 @@ app = MultiPage()
 
 # add applications
 app.add_page('Home', home.app)
-# app.add_page('Machine_Learning', machine_learning.app)
-app.add_page("KappaP",KappaP.app)
-app.add_page("AI4Kappa",AI4Kappa.app)
-app.add_page("CGCNN",CGCNN.app)
+app.add_page("KappaP", KappaP.app)
+app.add_page("AI4Kappa", AI4Kappa.app)
+
 # Run application
 if __name__ == '__main__':
     app.run()
